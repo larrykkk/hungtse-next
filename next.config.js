6c322d4a47d0a27117b95e40d1c16e95b2544190
larrykkk <xlarry1000@gmail.com>
2022-07-23 20:02:25 +0800

    調整 actions yml & next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  // basePath: "/",
-  // assetPrefix: "/larrykkk/",
+  basePath: isProd ? "/hungtse-next": '',
+  assetPrefix: isProd ? '/hungtse-next/' : '',
   reactStrictMode: true,
   images: {
     loader: "akamai",
