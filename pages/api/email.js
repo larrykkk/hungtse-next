e9afdca345016d9034a4600c6a8535e9a85d5ee6
larrykkk <xlarry1000@gmail.com>
2022-07-31 19:22:44 +0800

    add logger

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,11 +1,42 @@
 const nodemailer = require("nodemailer");
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
 export default async function handler(req, res) {
   var email = req.body.email;
   if (
-    email &&
-    Object.keys(email).length === 0 &&
-    Object.getPrototypeOf(email) === Object.prototype
+    !email ||
+    (email &&
+      Object.keys(email).length === 0 &&
+      Object.getPrototypeOf(email) === Object.prototype)
   ) {
     res.send("400");
     return;
@@ -14,7 +45,7 @@ export default async function handler(req, res) {
   var isError = false;
   await main(email).catch((err) => {
     isError = true;
-    console.error(err);
+    logger.error(err.message);
   });
 
   res.send(isError ? "error" : "success");
@@ -70,10 +101,10 @@ async function main(email) {
     html: html, // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
+  logger.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  logger.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
